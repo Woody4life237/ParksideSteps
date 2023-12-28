20675ee0ecf8ff01b31f2b0d5a80fbbb7abdde77
Woody4life237 <kieran.woodcock.93@gmail.com>
2023-12-28 23:59:54 +0000

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,214 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Checkbox, Slider, TextField } from "@mui/material";
+import { useState } from "react";
+import "./App.css";
+import Section from "./Components/Subject";
+import {
+  art,
+  computing_formal,
+  computing_preformal,
+  english,
+  food_tech,
+  global_studies,
+  maths,
+  music,
+  pe,
+  pshe_primary,
+  science,
+} from "./subjects";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+  const [showArt, setShowArt] = useState(true);
+  const [showComputingPreformal, setShowComputingPreformal] = useState(true);
+  const [showComputingFormal, setShowComputingFormal] = useState(true);
+  const [showEnglish, setShowEnglish] = useState(true);
+  const [showFoodTech, setShowFoodTech] = useState(true);
+  const [showGlobalStudies, setShowGlobalStudies] = useState(true);
+  const [showMaths, setShowMaths] = useState(true);
+  const [showMusic, setShowMusic] = useState(true);
+  const [showPE, setShowPE] = useState(true);
+  const [showPSHEPrimary, setShowPSHEPrimary] = useState(true);
+  const [showPSHESecondary, setShowPSHESecondary] = useState(true);
+  const [showScience, setShowScience] = useState(true);
+
+  const [stepsLevel, setStepsLevel] = useState([0, 20]);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setStepsLevel(newValue as number[]);
+  };
+
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setSearch(lowerCase);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+      <div>
+        | Art?:{" "}
+        <Checkbox
+          checked={showArt}
+          onChange={(e) => setShowArt(e.target.checked)}
+        />
+        | Computing (Preformal)?:{" "}
+        <Checkbox
+          checked={showComputingPreformal}
+          onChange={(e) => setShowComputingPreformal(e.target.checked)}
+        />
+        | Computing (Formal)?:{" "}
+        <Checkbox
+          checked={showComputingFormal}
+          onChange={(e) => setShowComputingFormal(e.target.checked)}
+        />
+        | English?:{" "}
+        <Checkbox
+          checked={showEnglish}
+          onChange={(e) => setShowEnglish(e.target.checked)}
+        />
+        | Food Tech?:{" "}
+        <Checkbox
+          checked={showFoodTech}
+          onChange={(e) => setShowFoodTech(e.target.checked)}
+        />
+        | Global Studies?:{" "}
+        <Checkbox
+          checked={showGlobalStudies}
+          onChange={(e) => setShowGlobalStudies(e.target.checked)}
+        />
+        | Maths?:{" "}
+        <Checkbox
+          checked={showMaths}
+          onChange={(e) => setShowMaths(e.target.checked)}
+        />
+        | Music?:{" "}
+        <Checkbox
+          checked={showMusic}
+          onChange={(e) => setShowMusic(e.target.checked)}
+        />
+        | PE?:{" "}
+        <Checkbox
+          checked={showPE}
+          onChange={(e) => setShowPE(e.target.checked)}
+        />
+        | PSHE (Primary)?:{" "}
+        <Checkbox
+          checked={showPSHEPrimary}
+          onChange={(e) => setShowPSHEPrimary(e.target.checked)}
+        />
+        | PSHE (Secondary)?:{" "}
+        <Checkbox
+          checked={showPSHESecondary}
+          onChange={(e) => setShowPSHESecondary(e.target.checked)}
+        />
+        | Science?:{" "}
+        <Checkbox
+          checked={showScience}
+          onChange={(e) => setShowScience(e.target.checked)}
+        />
+        |
+      </div>
+      <div className="slider">
+        Steps Levels
+        <Slider
+          value={stepsLevel}
+          onChange={handleChange}
+          valueLabelDisplay="on"
+          marks={true}
+          max={20}
+          min={1}
+        />
+      </div>
+      <div className="group">
+        {showArt && (
+          <Section search={search} obj={art} title="Art" steps={stepsLevel} />
+        )}
+        {showComputingPreformal && (
+          <Section
+            search={search}
+            obj={computing_preformal}
+            title="Computing (Preformal)"
+            steps={stepsLevel}
+          />
+        )}
+        {showComputingFormal && (
+          <Section
+            search={search}
+            obj={computing_formal}
+            title="Computing (Formal)"
+            steps={stepsLevel}
+          />
+        )}
+        {showEnglish && (
+          <Section
+            search={search}
+            obj={english}
+            title="English"
+            steps={stepsLevel}
+          />
+        )}
+        {showFoodTech && (
+          <Section
+            search={search}
+            obj={food_tech}
+            title="Food Tech"
+            steps={stepsLevel}
+          />
+        )}
+        {showGlobalStudies && (
+          <Section
+            search={search}
+            obj={global_studies}
+            title="Global Studies"
+            steps={stepsLevel}
+          />
+        )}
+        {showMaths && (
+          <Section
+            search={search}
+            obj={maths}
+            title="Maths"
+            steps={stepsLevel}
+          />
+        )}
+        {showMusic && (
+          <Section
+            search={search}
+            obj={music}
+            title="Music"
+            steps={stepsLevel}
+          />
+        )}
+        {showPE && (
+          <Section search={search} obj={pe} title="PE" steps={stepsLevel} />
+        )}
+        {showPSHEPrimary && (
+          <Section
+            search={search}
+            obj={pshe_primary}
+            title="PSHE - Primary"
+            steps={stepsLevel}
+          />
+        )}
+        {showScience && (
+          <Section
+            search={search}
+            obj={science}
+            title="Science"
+            steps={stepsLevel}
+          />
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Subject.tsx
@@ -0,0 +1,250 @@
+import { useEffect, useState } from "react";
+
+export default function Section({
+  title,
+  obj,
+  search,
+  steps,
+}: {
+  title: string;
+  obj: any;
+  search: string;
+  steps?: number[];
+}) {
+  function showLines(
+    lines: [],
+    subject: string,
+    category?: string,
+    subcategory?: string
+  ) {
+    let filteredLines = getLines(lines);
+
+    return filteredLines.map((line: string) => {
+      return (
+        <div
+          className="line"
+          onClick={() =>
+            copyToClipboard({
+              subject: subject,
+              category: category,
+              subcategory: subcategory,
+              line,
+            })
+          }
+        >
+          {line}
+        </div>
+      );
+    });
+  }
+
+  function copyToClipboard({
+    subject,
+    category,
+    subcategory,
+    line,
+  }: {
+    subject: string;
+    category?: string;
+    subcategory?: string;
+    line: string;
+  }) {
+    let headingEle = document.createElement("div");
+    let subheadingEle = document.createElement("div");
+    let subsubheadingEle = document.createElement("div");
+    let lineEle = document.createElement("div");
+
+    headingEle.textContent = subject;
+    headingEle.style.backgroundColor = "#8885c0";
+    headingEle.style.display = "inline-block";
+    headingEle.style.fontFamily = "Century Gothic";
+    document.body.appendChild(headingEle);
+
+    if (category) {
+      subheadingEle.textContent = category;
+      subheadingEle.style.backgroundColor = "#aaaaaa";
+      subheadingEle.style.display = "inline-block";
+      subheadingEle.style.fontFamily = "Century Gothic";
+      document.body.appendChild(subheadingEle);
+    }
+    if (subcategory) {
+      subsubheadingEle.textContent = subcategory;
+      subsubheadingEle.style.backgroundColor = "#ff99ff";
+      subsubheadingEle.style.display = "inline-block";
+      subsubheadingEle.style.fontFamily = "Century Gothic";
+      document.body.appendChild(subsubheadingEle);
+    }
+
+    lineEle.textContent = line;
+    lineEle.style.backgroundColor = "#66ccff";
+    lineEle.style.display = "inline-block";
+    lineEle.style.fontFamily = "Century Gothic";
+    document.body.appendChild(lineEle);
+
+    // Create a range object and select the combined content
+    var range = document.createRange();
+    range.selectNode(headingEle);
+    range.setEndAfter(lineEle); // Selects up to the end of the second element
+
+    // Add the selected content to the clipboard
+    var selection = window.getSelection();
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+
+    try {
+      // Execute the copy command
+      document.execCommand("copy");
+    } catch (err) {}
+
+    // Clear the selection
+    selection?.removeAllRanges();
+
+    // Remove the dummy elements from the body
+    document.body.removeChild(headingEle);
+    if (category) {
+      document.body.removeChild(subheadingEle);
+    }
+    if (subcategory) {
+      document.body.removeChild(subsubheadingEle);
+    }
+    document.body.removeChild(lineEle);
+  }
+
+  function getLines(lines: []) {
+    let filteredLines = lines.filter((line: string) => {
+      return !search || line.toLowerCase().includes(search);
+    });
+
+    return filteredLines;
+  }
+
+  function isShowStep(title: string) {
+    if (!steps) {
+      return true;
+    }
+    const stepRegex = /^Step (\d+)$/;
+
+    const match = title.match(stepRegex);
+
+    if (match) {
+      const stepNumber = parseInt(match[1], 10);
+      if (stepNumber >= steps[0] && stepNumber <= steps[1]) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+
+  function isShowSubject() {
+    let show = false;
+    if (!search && !steps) {
+      show = true;
+    } else {
+      Object.keys(obj).forEach((category) => {
+        if (isShowCategory(obj[category])) {
+          show = true;
+        }
+      });
+    }
+    return show;
+  }
+
+  function isShowCategory(category) {
+    let show = false;
+    if (!search && !steps) {
+      show = true;
+    } else if (Array.isArray(category)) {
+      category.forEach((line) => {
+        if (line.includes(search)) {
+          show = true;
+        }
+      });
+    } else {
+      Object.keys(category).forEach((subcategory) => {
+        if (isShowSubCategory(category[subcategory], subcategory)) {
+          show = true;
+        }
+      });
+    }
+    return show;
+  }
+
+  function isShowSubCategory(subcategory, name) {
+    let show = false;
+    if (!search && !steps) {
+      show = true;
+    }
+    if (steps) {
+      show = isShowStep(name);
+    }
+    if (search) {
+      show = false;
+      subcategory.forEach((line) => {
+        if (line.includes(search)) {
+          show = true;
+        }
+      });
+    }
+    return show;
+  }
+
+  return (
+    <div
+      className="group"
+      style={{ display: isShowSubject() ? "flex" : "none" }}
+    >
+      <div id={title} className="heading">
+        {title}
+      </div>
+      {Object.keys(obj).map((key) => {
+        return (
+          <div
+            className="group"
+            style={{ display: isShowCategory(obj[key]) ? "flex" : "none" }}
+          >
+            <div className="subheading">{key}</div>
+            {Array.isArray(obj[key])
+              ? showLines(obj[key], title, key)
+              : Object.keys(obj[key]).map((subheading) => {
+                  return (
+                    <div
+                      className="group"
+                      style={{
+                        display: isShowSubCategory(
+                          obj[key][subheading],
+                          subheading
+                        )
+                          ? "flex"
+                          : "none",
+                      }}
+                    >
+                      <div className="subsubheading">{subheading}</div>
+                      {Array.isArray(obj[key][subheading])
+                        ? showLines(
+                            obj[key][subheading],
+                            title,
+                            key,
+                            subheading
+                          )
+                        : Object.keys(obj[key][subheading]).map(
+                            (subsubheading) => {
+                              return showLines(
+                                obj[key][subheading][subsubheading],
+                                title,
+                                key,
+                                subheading
+                              );
+                            }
+                          )}
+                    </div>
+                  );
+                })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
