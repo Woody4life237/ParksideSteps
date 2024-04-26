4e7d11bce4161744f7be6e33eb181060e60d86cc
kwoodcock <kieran.woodcock@voicehost.co.uk>
2024-04-26 14:48:28 +0100

    Added hidden view counter
    Added hidden view counter
    Updated package info

--- src/App.tsx
+++ src/App.tsx
@@ -675,6 +675,7 @@ function App() {
           />
         )}
       </div>
+      <a style={{ display: "none" }} href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2FWoody4life237%2FParksideSteps"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2FWoody4life237%2FParksideSteps&countColor=%23263759" /></a>
       <div className="spacer"></div>
       <footer
         className="footer"
