bc072d51bf3b6e7dc0f661e0021cc98f663a42e0
Woody4life237 <kieran.woodcock.93@gmail.com>
2024-04-28 10:22:25 +0100

    Update App.tsx
    Fixed error with img lacking alt text

--- src/App.tsx
+++ src/App.tsx
@@ -675,7 +675,15 @@ function App() {
           />
         )}
       </div>
-      <a style={{ display: "none" }} href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2FWoody4life237%2FParksideSteps"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2FWoody4life237%2FParksideSteps&countColor=%23263759" /></a>
+      <a
+        style={{ display: "none" }}
+        href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2FWoody4life237%2FParksideSteps"
+      >
+        <img
+          src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2FWoody4life237%2FParksideSteps&countColor=%23263759"
+          alt="VistorBadge"
+        />
+      </a>
       <div className="spacer"></div>
       <footer
         className="footer"
